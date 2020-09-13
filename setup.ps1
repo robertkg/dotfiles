bb3dfb6c70c8402088247c097e0814b5e6abda30
robertkg <rgranmo@gmail.com>
2020-09-13 03:37:31 +0200

    setup mockup

---
+++ setup.ps1
@@ -0,0 +1,19 @@
+
+
+
+
+
+# set c:\tools dir
+# download win10 debloat
+# download oouso
+# install module get-childitemcolor
+# install chocolatey
+# install chocolatey packages
+# - choco list -l
+# - vim
+# - git
+# - pwsh 7
+# set vimrc
+# userstyles
+# - reddit minimal dark
+# pester test
