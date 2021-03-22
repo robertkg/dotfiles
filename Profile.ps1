8570cb2d018fa37d44738ad146ce6a33fd561f3d
Robert Kolstad Granmo <robert.kolstad.granmo@nhn.no>
2021-03-22 19:05:04 +0100

    Add functions, change prompt

--- Profile.ps1
+++ Profile.ps1
@@ -3,19 +3,82 @@ Set-PSReadlineOption -BellStyle None
 
 # Prompt
 
-function global:prompt {
-    Write-Host ("[$(($env:USERNAME).ToLower())@$(($env:COMPUTERNAME).ToLower())] ") -NoNewline -ForegroundColor Green
+function prompt {
+    # [username@machine] ~\myfolder:
+    # https://docs.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences
+    $esc = [char]27
     $regex = [regex]::Escape($HOME) + "(\\.*)*$"
-    Write-Host "$($executionContext.SessionState.Path.CurrentLocation.Path -replace $regex, '~$1'):" -NoNewline
-    return " "
+    "$ESC[32m[$(($env:USERNAME).ToLower())@$(($env:COMPUTERNAME).ToLower())]$esc[0m $($executionContext.SessionState.Path.CurrentLocation.Path -replace $regex, '~$1'): " 
 }
 
 # Functions
+## Git aliases for syncing branches
+function git-sync-test { git checkout test; git pull; git merge origin/qa --no-edit; git push }
+function git-sync-qa { git checkout qa; git pull; git merge origin/production --no-edit; git push }
+function git-sync { git checkout production; git pull; git-sync-qa; git-sync-test }
 ## List all certificates in local machine personal store. Format as it's a one-time use
 function Get-PersonalLmCerts {
     Get-ChildItem -Path "Cert:\LocalMachine\My" |
     Format-Table Subject, Thumbprint, NotAfter -AutoSize
 }
+
+
+function Get-CertificateExpiry {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true)]
+        [string[]] $Cn,
+        [Parameter(Mandatory = $true)]
+        [System.Management.Automation.Runspaces.PSSession] $Session
+    )
+
+    $certificates = Invoke-Command -Session $Session -ScriptBlock {
+        $certs = Get-ChildItem -Path "Cert:\LocalMachine\" -Recurse | Where-Object { $_.Subject -like "*$Using:Cn*" }
+        return $certs
+    }
+
+    return ($certificates | Select-Object Thumbprint, FriendlyName, NotBefore, NotAfter -Unique | Format-Table -AutoSize -Wrap)
+}
+
+function Get-SslThumbprint {
+    param(
+        [Parameter(
+            Position = 0,
+            Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true)
+        ]
+        [Alias('FullName')]
+        [String]$Url
+    )
+
+    Add-Type @"
+        using System.Net;
+        using System.Security.Cryptography.X509Certificates;
+            public class IDontCarePolicy : ICertificatePolicy {
+            public IDontCarePolicy() {}
+            public bool CheckValidationResult(
+                ServicePoint sPoint, X509Certificate cert,
+                WebRequest wRequest, int certProb) {
+                return true;
+            }
+        }
+"@
+    [System.Net.ServicePointManager]::CertificatePolicy = New-Object IDontCarePolicy
+
+    # Need to connect using simple GET operation for this to work
+    Invoke-RestMethod -Uri $URL -Method Get | Out-Null
+
+    $endpointRequest = [System.Net.Webrequest]::Create("$URL")
+    $sslThumbprint = $endpointRequest.ServicePoint.Certificate.GetCertHashString()
+
+    return [PSCustomObject] @{
+        Thumbprint = $sslThumbprint
+        Issuer     = $endpointRequest.ServicePoint.Certificate.Issuer
+        Subject    = $endpointRequest.ServicePoint.Certificate.Subject   
+    }
+}
+
 
 ## Update current user's PS profile with a given profile file
 function Update-PsProfile {
@@ -53,8 +116,8 @@ Set-Alias -Name "grep" -Value Select-String
 Set-Alias -Name "im" -Value Import-Module
 Set-Alias -Name "certlm" -Value Get-PersonalLmCerts
 Set-Alias -Name "esn" -Value Enter-PSSession
-Set-Alias -Name "ls" -Value Get-ChildItemColor -option AllScope -Force
-Set-Alias -Name "dir" -Value Get-ChildItemColor -option AllScope -Force
+Set-Alias -Name "ls" -Value Get-ChildItemColor -Option AllScope -Force
+Set-Alias -Name "dir" -Value Get-ChildItemColor -Option AllScope -Force
 
 # PSReadline key handlers
 ## Resolve the full path of the given relative path
