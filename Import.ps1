1101404f5a3e986b4d5d324d88cf0ae7c09f18cc
robertkg <rgranmo@gmail.com>
2021-03-22 20:05:11 +0100

    Update both profiles

--- Import.ps1
+++ Import.ps1
@@ -1,4 +1,3 @@
-#requires -runasadministrator
 
 [CmdletBinding()]
 param (
@@ -14,26 +13,24 @@ if (-not (Test-Path -Path $NewProfile)) {
     throw "Profile $NewProfile was not found."
 }
 
-
 $profileTemplate = (Get-Content -Raw -Path $NewProfile)
-$profilePath = "$env:USERPROFILE\Documents\WindowsPowerShell\Profile.ps1"
+$winProfilePath = "$env:USERPROFILE\Documents\WindowsPowerShell\profile.ps1"
+$coreProfilePath = "$env:USERPROFILE\Documents\PowerShell\profile.ps1"
 
-Write-Verbose "Importing profile `n`t- From: $NewProfile`n`t- To: $profilePath"
-
-if (-not (Test-Path -Path $profilePath)) {
-    Write-Verbose "Path $profilePath not found. Creating it..."
-    New-Item -ItemType File -Path $profilePath -Value $profileTemplate
+if (Test-Path -Path $winProfilePath) {
+    $profileTemplate > $winProfilePath
+    Write-Host "Updated $winProfilePath"
+}
+else {
+    New-Item $winProfilePath -Value $profileTemplate -Force
+    Write-Host "Created $coreProfilePath"
 }
 
-Set-Content -Path $profilePath -Value $profileTemplate
-
-# Link PowerShell Core profile
-if (Test-Path $env:USERPROFILE\Documents\PowerShell) {
-    Write-Verbose "Found path $env:USERPROFILE\Documents\PowerShell"
-    $coreProfilePath = "$env:USERPROFILE\Documents\PowerShell\Profile.ps1"
-    Write-Verbose "Creating symlink with Windows PowerShell profile.`n`t- From: $coreProfilePath`n`t- To: $profilePath"
-    New-Item -ItemType SymbolicLink -Path $coreProfilePath -Target $profilePath -Force 1> $null
+if (Test-Path -Path $coreProfilePath) {
+    $profileTemplate > $coreProfilePath
+    Write-Host "Updated $coreProfilePath"
 }
-
-Write-Output "Profile imported to '$profilePath'."
-
+else {
+    New-Item $winProfilePath -Value $profileTemplate
+    Write-Host "Created $coreProfilePath"
+}
